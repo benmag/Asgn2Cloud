1575926c6828995e411ac190dde4d674410a06cd
JoeMaher <joe.me@live.com.au>
2013-11-01 14:45:09 +1000

    s3, dynamo and simple all present

--- resources/library/storage.php
+++ resources/library/storage.php
@@ -2,14 +2,15 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-use Aws\SimpleDb\SimpleDbClient;
+//use Aws\SimpleDb\SimpleDbClient;
 use Aws\DynamoDb\DynamoDbClient;
 
 class storage { 
 
-	//var $bucketName; // name of the bucket we're storing stuff in
-	var $sdb; // simple db
+	//var $sdb; // simple db
         var $ddb; //dyanmo db
+        //var $s3; // S3
+        //var $bucketName; // name of the bucket we're storing stuff in
 
 	/** 
 	 * Get everything setup 
@@ -19,20 +20,25 @@ class storage {
         
 	function __construct() {
 
-		require_once(dirname(__FILE__)."/../../vendor/autoload.php");
-                require_once(dirname(__FILE__)."/../../config.php");
+            require_once(dirname(__FILE__)."/../../vendor/autoload.php");
+            require_once(dirname(__FILE__)."/../../config.php");
+            
+            // SimpleDB
+            /*$this->sdb = SimpleDbClient::factory(array(
+                'key'    => myAWSAccessKey,
+                'secret' => myAWSSecretKey,
+                'region' => 'sa-east-1'
+            ));*/
 
-		/*$this->sdb = SimpleDbClient::factory(array(
-                    'key'    => myAWSAccessKey,
-                    'secret' => myAWSSecretKey,
-                    'region' => 'us-west-2'
-                ));*/
-                
-                $this->ddb = DynamoDbClient::factory(array(
-                    'key'    => myAWSAccessKey,
-                    'secret' => myAWSSecretKey,
-                    'region' => 'us-west-2'
-                ));
+            $this->ddb = DynamoDbClient::factory(array(
+                'key'    => myAWSAccessKey,
+                'secret' => myAWSSecretKey,
+                'region' => 'sa-east-1'
+            ));
+            
+            // S3
+            /*$this->bucketName = BUCKET_NAME;
+            $this->s3 = new S3(myAWSAccessKey, myAWSSecretKey); */
 
 	}
 
@@ -40,33 +46,39 @@ class storage {
 	 * Save tweet into a bucket 
 	 *
 	 * @param $tweetData - array containing all the tweet data we want to store
-	 * @return string S3 storage location
 	 */
         
 	public function put_tweet($searchTerm, $tweetData) {
 
-                /*$this->sdb->putAttributes(array(
-                    'DomainName' => $this->getDomain(),
-                    'ItemName'   => 'tweet' . $this->generateUniqueTweetId(),
-                    'Attributes' => array(
-                        array('Name' => 'text', 'Value' => $tweetData['text']),
-                        array('Name' => 'sentiment', 'Value' => $tweetData['sentiment']),
-                        array('Name' => 'createdA', 'Value' => $tweetData['createdAt']),
-                        array('Name' => 'screenName', 'Value' => $tweetData['screenName']),
-                        array('Name' => 'name', 'Value' => $tweetData['name']),
-                        array('Name' => 'profileImageUrl', 'Value' => $tweetData['profileImageUrl']),
-                        array('Name' => 'searchTerm', 'Value' => $tweetData['searchTerm'])
-                    )
-                ));*/
+            // SimpleDB
+            /*$this->sdb->putAttributes(array(
+                'DomainName' => $this->getDomain(),
+                'ItemName'   => 'tweet' . $this->generateUniqueTweetId(),
+                'Attributes' => array(
+                    array('Name' => 'text', 'Value' => $tweetData['text']),
+                    array('Name' => 'sentiment', 'Value' => $tweetData['sentiment']),
+                    array('Name' => 'createdA', 'Value' => $tweetData['createdAt']),
+                    array('Name' => 'screenName', 'Value' => $tweetData['screenName']),
+                    array('Name' => 'name', 'Value' => $tweetData['name']),
+                    array('Name' => 'profileImageUrl', 'Value' => $tweetData['profileImageUrl']),
+                    array('Name' => 'searchTerm', 'Value' => $tweetData['searchTerm'])
+                )
+            ));*/
+            
+            // DynamoDB
             $time = time();
 
             $this->ddb->putItem(array(
                 'TableName' => 'joeTweets',
                 'Item' => array(
-                    'id'      => array('N' => '1201'),
-                    'time'    => array('N' => $time)
+                    'id'      => array('S' => $this->generateUniqueTweetId()), //im not sure about this, but everythign ive read says the index should be random
+                    'time'    => array('N' => $time) //allows for easier querying if we ever need it
             )
             ));
+            
+            // s3
+            /*$uploadName = $searchTerm . "/tweet_" . $this->generateUniqueTweetId() . ".json";
+            $this->s3->putObject(json_encode($tweetData), $this->bucketName, $uploadName, S3::ACL_PUBLIC_READ, array(), array('Content-Type' => 'text/plain'));*/
 	}
 
 
@@ -78,7 +90,7 @@ class storage {
         
 	private function generateUniqueTweetId() {
 	 	
-	 	// List of characters
+            // List of characters
 	    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 	    $randomString = '';
 
@@ -90,10 +102,11 @@ class storage {
 	    return $randomString;
 	}
         
-        private function getDomain(){
+        // simpleDb
+        /*private function getDomain(){
             $int = rand(1, 25);
             return 'tweets' . $int;
-        }
+        }*/
 
 
 }

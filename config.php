b2ae2da70d91cefc9e2dfe160321e2cc05e783ee
JoeMaher <joe.me@live.com.au>
2013-11-04 17:59:41 +1000

    Updated config

--- config.php
+++ config.php
@@ -4,13 +4,17 @@
 define('SITE_ADDRESS', "http://".$_SERVER['SERVER_NAME']);
 
 // only one instance needs to run the node part. The other instances just handle the sentiment calls
-define('NODE_SERVER', "http://54.200.160.95:8080"); 
+define('NODE_SERVER', "http://54.200.86.66:8080"); 
 
 // What bucket do you want to store the tweets in?
-define('BUCKET_NAME', 'benM');
+define('BUCKET_NAME', 'joeMaher');
 
-// Amazon keys
+// Amazon keys and region
 define('myAWSAccessKey', 'AKIAJHPGFCQYS4DZU35Q');
 define('myAWSSecretKey', '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');
+define('region', 'us-west-2');
+
+//dynamo db table name
+define('tableName', 'joeTweets');
 
 ?>
